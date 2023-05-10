357effe0222d56d8a27a828e609e1b5c3ea64a33
Sagar More <animsh.more@gmail.com>
2023-05-10 16:00:55 +0530

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,40 +24,49 @@ const theme = createTheme({
 
 const teamMembers = [
   {
-    name: "Member 1",
-    role: "Role 1",
-    image: "https://via.placeholder.com/150",
-    college: "College 1",
+    name: "Prof. S. V. Shinde",
+    role: "Project Guide",
+    image: "https://media.licdn.com/dms/image/C4E03AQEwvlLqKDHDPg/profile-displayphoto-shrink_200_200/0/1616044305241?e=1689206400&v=beta&t=KcBQNcQgrNyuTcfxvrOuinKW-A-5XOFyOOryDHej5Bs",
+    college: "PDEA's College of Engineering, Manjari (BK), Pune",
     github: "",
-    linkedin: "",
+    linkedin: "https://www.linkedin.com/in/sach-shinde-6686bb209/",
     gmail: "",
   },
   {
-    name: "Member 2",
-    role: "Role 2",
-    image: "https://via.placeholder.com/150",
-    college: "College 1",
-    github: "",
-    linkedin: "",
-    gmail: "",
+    name: "Sagar More",
+    role: "Developer",
+    image: "https://avatars.githubusercontent.com/u/68194471?s=400&u=e63d886edf29c34d12f7bb090a9a04a656c5160d&v=4",
+    college: "PDEA's College of Engineering, Manjari (BK), Pune",
+    github: "https://github.com/animsh",
+    linkedin: "https://www.linkedin.com/in/animsh/",
+    gmail: "animsh.more@gmail.com",
   },
   {
-    name: "Member 3",
-    role: "Role 3",
-    image: "https://via.placeholder.com/150",
-    college: "College 1",
-    github: "",
-    linkedin: "",
-    gmail: "",
+    name: "Pravin Bhagwat",
+    role: "Developer",
+    image: "https://avatars.githubusercontent.com/u/85389806?v=4",
+    college: "PDEA's College of Engineering, Manjari (BK), Pune",
+    github: "https://github.com/pravinbhagwat",
+    linkedin: "https://www.linkedin.com/in/pravin-bhagwat/",
+    gmail: "pravinbhagwat150@gmail.com",
   },
   {
-    name: "Member 4",
-    role: "Role 4",
-    image: "https://via.placeholder.com/150",
-    college: "College 1",
+    name: "Indrajeet Karande",
+    role: "Technical Researcher",
+    image: "https://media.licdn.com/dms/image/D4D03AQH-ImMTREVZRg/profile-displayphoto-shrink_200_200/0/1678798701155?e=1689206400&v=beta&t=0whLAwKT_F-uDdojDocNSY4lIik29rivvBlOJFkUQdY",
+    college: "PDEA's College of Engineering, Manjari (BK), Pune",
     github: "",
-    linkedin: "",
-    gmail: "",
+    linkedin: "https://www.linkedin.com/in/indrajeet-karande-82879b1b1/",
+    gmail: "indrajeetkarande4444@gmail.com",
+  },
+  {
+    name: "Sayaji Dhandge",
+    role: "Techniocal Researcher",
+    image: "https://media.licdn.com/dms/image/D4D03AQG5tp4P0qBO3Q/profile-displayphoto-shrink_200_200/0/1672595665040?e=1689206400&v=beta&t=sgQA47qm4rkhwFOwLmeSspLTSW5f-dSVbM4F4OvSVkM",
+    college: "PDEA's College of Engineering, Manjari (BK), Pune",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/sayaji-dhandge-6261b0222/",
+    gmail: "sayajidhandge02@gmail.com",
   },
 ];
 const About = () => {
@@ -85,9 +94,8 @@ const About = () => {
             Project Details
           </Typography>
           {/* Add your project details here */}
-          <Typography variant="body1" gutterBottom>
-            This is a sample project with a team of four members.
-          </Typography>
+          <Typography variant="body1" textAlign={'justify'} gutterBottom>
+            The "Predictive Analysis of Student Grade and Career" project is designed specifically for computer engineering students at SPPU (Savitribai Phule Pune University). It utilizes student data and questionnaires to forecast potential outcomes for end-semester grades and career paths. It should be noted that this project provides predictions based on the available data and questionnaires, but real-life outcomes may vary for individual students. While the project aims to assist students by offering insights into potential future possibilities, the actual results may differ.          </Typography>
 
           <Typography variant="h5" component="h3" gutterBottom>
             Meet the Team
@@ -111,19 +119,22 @@ const About = () => {
                           {member.name}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                          {member.college}
+                          {member.role}, {member.college}
                         </Typography>
                       </Grid>
                       <Grid item>
-                        <IconButton href={member.github} target="_blank">
+                        {member.github !== '' && <IconButton href={member.github} target="_blank">
                           <GitHub />
                         </IconButton>
-                        <IconButton href={`mailto:${member.gmail}`}>
+                        }
+                        {member.gmail !== '' && <IconButton href={`mailto:${member.gmail}`}>
                           <Email />
                         </IconButton>
-                        <IconButton href={member.linkedin} target="_blank">
+                        }
+                        {member.linkedin !== '' && <IconButton href={member.linkedin} target="_blank">
                           <LinkedIn />
                         </IconButton>
+                        }
                       </Grid>
                     </Grid>
                   </CardContent>
